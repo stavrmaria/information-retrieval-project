b706bacc5a78d09ae98013cc1ea7d931aeb4d50b
Stavrianopoulou Maria <stavrianopoulou.maria@gmail.com>
2023-11-14 17:51:29 +0200

    update: file
    Added new endpoints for queries and tf-idf

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 import sys
-import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import pandas as pd
@@ -12,8 +11,6 @@ def process_text(text):
     translator = str.maketrans("", "", string.punctuation)
     text = text.translate(translator)
 
-    print(text, file=sys.stderr)
-
     stop = set(stopwords.words('greek') + list(string.punctuation))
     tokens = [i for i in word_tokenize(text.lower()) if i not in stop]
     stemmed_tokens = [greek_stemmer.stemWord(token) for token in tokens]
@@ -22,19 +19,15 @@ def process_text(text):
 # Save the inverted index to the JSON file
 def save_index(inverted_index, json_file_path):
     with open(json_file_path, 'w', encoding='utf-8') as json_file:
-        json.dump(inverted_index, json_file, ensure_ascii=False)
+        json.dump(inverted_index, json_file, ensure_ascii=False, indent=4)
 
 def construct_inverted_index(data_file_path):
     inverted_index = {}
-    nltk.download('punkt')
-    nltk.download('stopwords')
-
     # Read the speech column of the csv file
     df_speeches = pd.read_csv(data_file_path, usecols = ['speech'])
 
     # Get the speech rows of the csv file
     for doc_id, row in df_speeches.iterrows():
-        
         # Tokenize and preprocess the speech
         speech = row['speech']
         tokens = process_text(speech)
@@ -42,14 +35,12 @@ def construct_inverted_index(data_file_path):
         # Construct the index based on the tokens of each speech
         for term in tokens:
             if term in inverted_index:
-                if doc_id in inverted_index[term][1]:
-                    # Increase n_t of the term and update the (doc_id, freq) entry
-                    inverted_index[term][1][doc_id] += 1
+                if doc_id in inverted_index[term]:
+                    inverted_index[term][doc_id] += 1
                 else:
-                    inverted_index[term][1][doc_id] = 1
-                    inverted_index[term][0] += 1
+                    inverted_index[term][doc_id] = 1
             else:
                 entries = {doc_id: 1}
-                inverted_index[term] = [1, entries]
+                inverted_index[term] = entries
     
     return inverted_index
--- views.py
+++ views.py
@@ -1,15 +1,21 @@
-from flask import Blueprint, Response, render_template
+from flask import Blueprint, Response, jsonify, render_template, request
 import json
 import os
-from index import construct_inverted_index, save_index
+from index import construct_inverted_index, save_index, process_text
+from vectorizer import calculate_tf_idf, save_tf_idf
 
 DATA_FILE = 'sample_data.csv'
 INDEX_FILE = 'inverted_index.json'
+TFIDF_FILE = 'tf_idf_index.json'
 DATA_FOLDER = 'data'
 
 # Get the data file path
 current_path = os.getcwd()
 csv_file_path = os.path.join(os.path.join(os.getcwd(), DATA_FOLDER), DATA_FILE)
+index_file_path = os.path.join(os.path.join(os.getcwd(), DATA_FOLDER), INDEX_FILE)
+tfidf_file_path = os.path.join(os.path.join(os.getcwd(), DATA_FOLDER), TFIDF_FILE)
+
+content_type='application/json; charset=utf-8'
 views = Blueprint(__name__, "views")
 
 @views.route('/')
@@ -19,14 +25,44 @@ def index():
 @views.route('/get_index')
 def get_index():
     # Load the inverted index from the JSON file
-    index_file_path = os.path.join(os.getcwd(), INDEX_FILE)
     if not os.path.exists(index_file_path):
         inverted_index = construct_inverted_index(csv_file_path)
-        save_index(inverted_index, INDEX_FILE)
+        save_index(inverted_index, index_file_path)
 
-    with open('inverted_index.json', 'r', encoding='utf-8') as json_file:
+    with open(index_file_path, 'r', encoding='utf-8') as json_file:
         inverted_index = json.load(json_file)
 
     # Serialize the data using json.dumps to avoid Unicode escape sequences
-    response = Response(json.dumps(inverted_index, ensure_ascii=False), content_type='application/json; charset=utf-8')
+    response = Response(json.dumps(inverted_index, ensure_ascii=False), content_type=content_type)
     return response
+
+@views.route('/get_tfidf')
+def get_tftidf():
+    # Load the inverted index from the JSON file
+    if not os.path.exists(index_file_path):
+        get_index()
+    
+    # Load the tf-idf index from the JSON file
+    if not os.path.exists(tfidf_file_path):
+        tf_idf_index = calculate_tf_idf(csv_file_path)
+        save_tf_idf(tf_idf_index, tfidf_file_path)
+
+    with open(tfidf_file_path, 'r', encoding='utf-8') as json_file:
+        tf_idf_index = json.load(json_file)
+
+    # Serialize the data using json.dumps to avoid Unicode escape sequences
+    response = Response(json.dumps(tf_idf_index, ensure_ascii=False), content_type=content_type)
+    return response
+
+@views.route('/query', methods=['GET'])
+def query():
+    try:
+        # Get the query from the URL parameters
+        user_query = request.args.get('q')
+        if not user_query:
+            raise ValueError('Query parameter is missing.')
+
+        response = process_text(user_query)
+        return response
+    except Exception as e:
+        return jsonify({"error": str(e)})
