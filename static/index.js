190cca50d7e40fe5170b9b441f9a6364e86a5d5c
Stavrianopoulou Maria <stavrianopoulou.maria@gmail.com>
2023-11-17 19:20:16 +0200

    update: file
    Added themes

--- static/index.js
+++ static/index.js
@@ -43,4 +43,22 @@ function selectInput(list) {
 function toggleTheme() {
     const body = document.body;
     body.classList.toggle('dark-mode');
+
+    // Check if dark mode is enabled and save the preference to localStorage
+    const isDarkModeEnabled = body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkModeEnabled);
 }
+
+// Check the saved theme preference on page load
+document.addEventListener('DOMContentLoaded', () => {
+    const isDarkModeEnabled = localStorage.getItem('darkMode') === 'true';
+
+    // Apply the saved theme preference
+    const body = document.body;
+    if (isDarkModeEnabled) {
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+    }
+});
+  
---
+++ static/results.js
@@ -0,0 +1,22 @@
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+
+    // Check if dark mode is enabled and save the preference to localStorage
+    const isDarkModeEnabled = body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkModeEnabled);
+}
+
+// Check the saved theme preference on page load
+document.addEventListener('DOMContentLoaded', () => {
+    const isDarkModeEnabled = localStorage.getItem('darkMode') === 'true';
+
+    // Apply the saved theme preference
+    const body = document.body;
+    if (isDarkModeEnabled) {
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+    }
+});
+  
